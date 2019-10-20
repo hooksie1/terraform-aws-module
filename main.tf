43c62425e7e233a1f98424e46637640c91423c2a
John Hooks <hooksie11@gmail.com>
2019-10-19 21:18:42 -0400

    Fix data name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "cloudflare_zones" "default" {
 }
 
 resource "cloudflare_record" "a_record" {
-    zone_id     = cloudflare_zone.default.id
+    zone_id     = cloudflare_zones.default.id
     name        = var.name
     value       = aws_instance.vm.public_ip
     type        = "A"
