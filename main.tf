dd43c5bb4b745d4f20e23577aa4e9765ba039b25
John Hooks <hooksie11@gmail.com>
2019-10-19 20:50:48 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "vm" {
+    count           = var.number
+    ami             = var.image
+    instance_type   = var.size
+    key_name        = var.key
+
+
+    tags = {
+        Name = var.name
+    }
+}
+
+resource "cloudflare_record" "a_record" {
+    zone_id     = var.cloudflare_zone
+    name        = var.name
+    value       = aws_instance.vm.public_ip
+    type        = "A"
+}
---
+++ vars.tf
@@ -0,0 +1,5 @@
+variable name   {}
+variable size   {}
+variable image  {}
+variable number {}
+variable key    {}
